30ce62742d1095fe5207627a9e6cc381b1991da5
shohan12727 <aishohan001@gmail.com>
2025-04-11 12:43:55 +0600

    object handle example done

--- src/component/MyComponent.jsx
+++ src/component/MyComponent.jsx
@@ -1,20 +1,32 @@
 
 import React, {useState} from "react";
 function MyComponent () {
-    const [car,Setcar] = useState({
+    const [car,setCar] = useState({
         year:2024,
         make:"Ford",
         model:"mustang"
     });
+
+    function handleYearChange (event) {
+        setCar(c => ({...c, year: event.target.value}));
+        
+    }
+    function handleMakeChange (event) {
+        setCar( c => ({...c, make: event.target.value}));
+
+    }
+    function handleModelChange (event) {
+        setCar ( c => ({...c, model: event.target.value}));
+
+    }
     
-
-
     return(
-        <div>
-            <p>Your favourite car is : {car.year}
-                                       {car.make}
-                                       {car.model}
+        <div className="car-container">
+            <p className="title-container">Your favourite car is : {car.year} {car.make} {car.model}
             </p>
+            <input className="year-input-container" type="number" value={car.year} onChange={handleYearChange} /> <br />
+            <input className="make-input-container" type="text" value={car.make} onChange={handleMakeChange} /> <br />
+            <input className="model-input-container" type="ntext" value={car.model} onChange={handleModelChange} />
         </div>
 
     );
